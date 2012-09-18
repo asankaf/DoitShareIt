470db22e0eee774efbfc5068639533d2eb92ed4a
Roshan Dhananjaya <roshandhananjaya@gmail.com>
2012-09-18 11:31:21 +0530

    folders

---
+++ DSH.Main.Web/Boilerplate/src/modules/newsFeedModule/uiDemo/component.js
@@ -0,0 +1,45 @@
+﻿define(['Boiler', 'text!./view.html', './postingPanel/component', './demo1/component'],
+    function(Boiler, template, PostingPanelComp, Demo1Component) {
+
+        /**
+    * Parent Component class that will hold the clickme and lottery components
+    * @class 
+    * @param moduleContext {Boiler.Context} 
+    */
+        var uiDemoComponent = function(moduleContext) {
+            var self = this;
+
+
+            var parentPanel = null;
+
+
+            this.activate = function(parent, params) {
+                if (!parentPanel) {
+                    //create the holding panel for clickme and lottery components
+                    parentPanel = new Boiler.ViewTemplate(parent, template, null);
+
+
+                    /* 
+                    //create the clickme component and append to the parent
+                    var postingPanelComp = new PostingPanelComp(moduleContext);
+                    postingPanelComp.initialize($('#postingPanel'));
+                    */
+                //create a demo1 component and add to the parent
+                    var demo1Component = new Demo1Component(moduleContext);
+                    demo1Component.initialize($('#demo1'));
+
+
+                }
+                parentPanel.show();
+            };
+            this.deactivate = function() {
+                if (parentPanel) {
+                    parentPanel.hide();
+                }
+
+            };
+        };
+
+        return uiDemoComponent;
+
+    });
---
+++ DSH.Main.Web/Boilerplate/src/modules/newsFeedModule/uiDemo/demo1/viewmodel.js
@@ -0,0 +1,15 @@
+﻿define([], function () {
+
+
+    var ViewModel = function (moduleContext) {
+        var self = this;
+
+
+        self.val = ko.observable(50);
+        self.disabled = ko.observable(false);
+        
+
+    };
+
+    return ViewModel;
+});
