6987eeb05e36de1a6d0adb39362ce5e8a5736012
Roshan Dhananjaya <roshandhananjaya@gmail.com>
2012-09-24 11:14:14 +0530

    partially adding models for auto mapper

---
+++ DSH.Access/PostsAccess/Model/PostHistories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSH.Access.UserAccess.Model
+{
+    [Serializable]
+    public class PostHistories
+    {
+    }
+}
---
+++ DSH.Access/UserAccess/Model/Badges.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSH.Access.UserAccess.Model
+{
+    [Serializable]
+    public class Badges
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
