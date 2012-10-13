defd331fbc7b2b692a41f7ef0a7efe3583bf22a8
Supun Nakandala <supun.nakandala@gmail.com>
2012-10-13 04:52:31 -0700

    Error Correction

--- DSH.Main.Web/Boilerplate/src/modules/newsFeedModule/newsFeed/component.js
+++ DSH.Main.Web/Boilerplate/src/modules/newsFeedModule/newsFeed/component.js
@@ -1,4 +1,4 @@
-﻿define(['Boiler', 'text!./view.html', './postingPanel/component', './publicWall/component'], function (Boiler, template, PostingPanelComp, PublicWallComp) {
+﻿define(['Boiler', 'text!./view.html', './publicWall/component'], function (Boiler, template, PublicWallComp) {
 
     /**
     * Parent Component class that will hold the clickme and lottery components
@@ -13,17 +13,13 @@
             if (!parentPanel) {
                 //create the holding panel for clickme and lottery components
                 parentPanel = new Boiler.ViewTemplate(parent, template, null);
-                //create the clickme component and append to the parent
-                var postingPanelComp = new PostingPanelComp(moduleContext);
-                postingPanelComp.initialize($('#postingPanel'));
-                //create lottery component and add to the parent
                 var publicWallComp = new PublicWallComp(moduleContext);
                 publicWallComp.initialize($('#publicWall'));
             }
             parentPanel.show();
         };
 
-        this.deactivate = function() {
+        this.deactivate = function () {
             if (parentPanel) {
                 parentPanel.hide();
             }
