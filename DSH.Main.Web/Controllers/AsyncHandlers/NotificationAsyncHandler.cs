c0ca798f0cf9a1013148749e68c2b1dd181e1f17
Supun Nakandala <supun.nakandala@gmail.com>
2012-10-14 20:07:47 -0700

    Added NotificationAsyncController.cs which has the capability of server pushing(reverse ajax) functionality Added NotificationAsyncHandler to handle notification handling

---
+++ DSH.Main.Web/Controllers/AsyncHandlers/NotificationAsyncHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace DSH.Main.Web.Controllers.AsyncHandlers
+{
+    public class NotificationAsyncHandler
+    {
+        public delegate void CheckForNotificationResponse();
+
+        public IAsyncResult CheckForNotificationAsync(CheckForNotificationResponse resp)
+        {
+            return new MyAsyncResult(resp);
+        }
+
+        private class MyAsyncResult : IAsyncResult
+        {
+            private readonly CheckForNotificationResponse _mResp;
+
+            public MyAsyncResult(CheckForNotificationResponse resp)
+            {
+                _mResp = resp;
+                var thread = new Thread(new ThreadStart(() =>
+                                                               {
+                                                                   Thread.Sleep(30000);
+                                                                   _mResp();
+                                                               }));
+                thread.Start();
+            }
+
+            public bool IsCompleted { get; private set; }
+            public WaitHandle AsyncWaitHandle { get; private set; }
+            public object AsyncState { get; private set; }
+            public bool CompletedSynchronously { get; private set; }
+        }
+    }
+}
