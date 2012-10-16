bbf3c6b77100eb7da712e20a939bca6a43a0fd81
rdhananjaya <roshandhananjaya@gmail.com>
2012-10-16 11:10:18 -0700

    anonymous picture toggling

--- DSH.Main.Web/Boilerplate/src/modules/selectedUserModule/selectedUser/selectedUserPostingPanel/viewmodel.js
+++ DSH.Main.Web/Boilerplate/src/modules/selectedUserModule/selectedUser/selectedUserPostingPanel/viewmodel.js
@@ -1,7 +1,12 @@
 ﻿define([], function () {
     var viewModel = function (moduleContext) {
         var self = this;
+        var isAnonymous = false;
+
         this.postText = ko.observable("");
+
+        this.picLocation = ko.observable("");
+
         this.makePost = function (formElement) {
             if (formElement.elements["post"].value == '') {
                 alert('please enter a valid message');
@@ -10,15 +15,45 @@
                     type: "POST",
                     url: "/Post/Create",
                     dataType: "json",
-                    data: { Body: $('<div/>').text(formElement.elements["post"].value).html(), PostTypeId: 2 },
+                    data: {
+                        Body: $('<div/>').text(formElement.elements["post"].value).html(),
+                        PostTypeId: 2,
+                        IsAnonymous: isAnonymous
+                    },
                     success: function (result) {
                         if (result.Status == "SUCCESS") {
                             moduleContext.notify('NEW_POST', result.Result.Data);
+                            //                            alert("Success!!!!");
                         }
                     }
                 });
             }
         };
+
+        this.anonymousToggle = function (clickedElement) {
+            if (isAnonymous) {
+                isAnonymous = false;
+                self.picLocation(self.picUrl);
+            } else {
+                isAnonymous = true;
+                self.picLocation("../../Boilerplate/src/modules/baseModule/theme/gray/bullet1.png");
+            }
+
+            /*alert("isAnonymous: " + isAnonymous);*/
+
+        };
+
+        var picLocation = "";
+        this.wchickPicture = 0;
+
+        // getting the profile picture url from the display module
+        this.picUrl = moduleContext.retreiveObject("profilePicURL");
+
+        // the posting panel will show the pic of you as default: ( default is non anonymous posting) 
+        this.picLocation(picUrl);
+
+
+
 
     };
 
